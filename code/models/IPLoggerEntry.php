174590567d184c85692cee8650ad599d27116c61
Charles Ritchie <charles.w.ritchie@gmail.com>
2017-02-26 16:21:13 +1300

    Add summary_fields and default_sort.

--- code/models/IPLoggerEntry.php
+++ code/models/IPLoggerEntry.php
@@ -8,7 +8,14 @@ class IPLoggerEntry extends DataObject
 {
     private static $db = array(
         'Event'   => 'Varchar(255)',
-        'IP'      => 'Varchar(255)',
-        'Expried' => 'Boolean'
+        'IP'      => 'Varchar(255)'
+    );
+
+    private static $default_sort = 'Created';
+
+    private static $summary_fields = array(
+        'Created',
+        'IP',
+        'Event'
     );
 }
